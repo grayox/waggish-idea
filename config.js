8647fb2587cc07e7756e3a4e14efbfa4f7daaf9e
Gray Ox <grayox@example.com>
2021-05-08 18:51:00 -0700

    works

--- config.js
+++ config.js
@@ -74,7 +74,7 @@ const WAIT = {
 // const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36';
 const userAgentObject = new UserAgent();
 const userAgent = userAgentObject.toString();
-console.log(`userAgent: ${ userAgent }`);
+// console.log(`userAgent: ${ userAgent }`);
 
 // ---------------- [ BEGIN ] options for anonymous scraping compliance ----------------
 //                             ( @comment out for testing )
